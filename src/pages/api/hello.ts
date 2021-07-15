f1d66bd5703fc619378c813b4a3f766fb25a3119
Andrey Prisniak <aprisniak@lectrum.io>
2021-07-15 20:21:15 +0300

    fix(source): fix api example

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,16 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
-import bcrypt from 'bcryptjs';
-import jwt from 'formik';
-import jwt1 from 'react-hook-form';
 
 type Data = {
   name: string
 }
 
 export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: bcrypt.hashSync('John Doe', 11) })
+    res.status(200).json({ name: 'John Doe' });
 }
